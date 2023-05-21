7184326de74e209c8604c7a50a1a81e29ea29c6f
unknown <Max54611>
2023-05-21 18:48:25 -0500

    Subiendo proyecto

---
+++ src/test/java/com/a/ventafrutos/VentaFrutosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.a.ventafrutos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VentaFrutosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
